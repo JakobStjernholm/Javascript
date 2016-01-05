bb718c623863dd94c2a9c4161fc6c183aabdd0e6
Jakob Stjernholm <jakob.stjerna@gmail.com>
2016-01-05 10:44:19 +0100

    Done things, search method

--- Lesson06/Scripts/index.js
+++ Lesson06/Scripts/index.js
@@ -1,1 +1,27 @@
-﻿
+﻿$(function () {
+    $('#search').keyup(function () {
+        var searchField = $('#search').val();
+        var myExp = new RegExp(searchField, "i");
+        $.getJSON('wt_data.json', function (data) {
+            var output = "<ul class='searchresults'>";
+
+            $.each(data, function (key, val) {
+                if ((val.name.search(myExp) !== -1) || (val.about.search(myExp) !== -1)) {
+                    output += "<li>";
+                    output += "<p>";
+                    output += "<strong>" + val.name + "</strong>";
+                    output += val.master_degree;
+                    output += "<br><img alt='" + val.name + "' class='img-rounded img' src='\Images/" + val.url_image + "'>";
+                    output += "</p>";
+                    output += "</li>";
+                }
+
+            });
+            output += "</ul>";
+            $('#update').html(output);
+            $(".img").error(function () {
+                $(this).attr('src', '\Images/suddigt-full.PNG');
+            });
+        });
+    });
+});

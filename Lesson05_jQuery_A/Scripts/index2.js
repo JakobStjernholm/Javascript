1ab48b63dd932eeff14d5ebb6354f24660be4b3b
Jakob Stjernholm <jakob.stjerna@gmail.com>
2016-01-05 08:44:43 +0100

    commit

--- Lesson05_jQuery_A/Scripts/index2.js
+++ Lesson05_jQuery_A/Scripts/index2.js
@@ -1,4 +1,12 @@
 ﻿$(document).ready(function() {
-    var element = "Pris: 399 kr"; //Bad
-    var element2 = "<p>Pris: 399 kr </p>"; //Bad
+    //var element = "Pris: 399 kr"; //Bad
+    //var element = "<p>Pris: 399 kr </p>"; //Bad
+    //Skapar ett element men lägger inte till det i DOM-trädet
+    var element = $("<p>Pris: 399 kr </p>"); //Good
+    //.append(),   .prepend(),   .after(),       .before()
+    //.appendTo(), .prependTo(), .insertAfter(), .insertBefore();
+    $('#paskmat p:first').append(element);
+    $('<span>Hej</span>').appendTo($('#paskmat'));
+    $('button').remove(); //Tar bort alla knappar
+    
 });
---
+++ Lesson05_jQuery_A/Scripts/index3.js
@@ -0,0 +1,14 @@
+﻿$(function() {
+    $('button').on('click', function() {
+        //Körs när knappen tryckts
+        var price = $('<p>39.90 kr/kg</p>');
+        $(this).after(price);
+        this.remove();
+        //Hämta <ul>-elementet
+        //$(this).parent().parent().append(price);
+
+        //Hämtar alla föräldrar och farföräldrar osv som är desert
+        //$(this).parents('.desert').append(price);
+        $(this).closest('.dinner').append(price); //Bästa sättet att hämta ut, hämtar ur ett element
+    });
+});
